9aed7b83fcb1db4af0693558efce53b218649c42
Anurag Singh <78840945+anuragsingh17ai@users.noreply.github.com>
2022-05-14 19:42:24 +0000

    Update eating_queries.cpp

--- eating_queries.cpp
+++ eating_queries.cpp
@@ -1,29 +1,38 @@
 #include<bits/stdc++.h>
 #define f(i,a,b) for(int i=a;i<b;i++)
 using namespace std;
-
+const int N=2e5+5;
+int s[N]; 
+int cmp(int x,int y)
+{
+	return x>y;
+}
 int main()
 {
 	int t;
 	cin>>t;
 	while(t--)
 	{
-		long long n,q;
+		int n,q;
 		cin>>n>>q;
 		int v[n];
 		f(i,1,n+1)
 			cin>>v[i];
-		sort(v,v+n+1);
+		sort(v,v+n+1,cmp);
+		
+		f(i,1,n+1) s[i]=s[i-1]+v[i];
+		
 		
 		while(q--)
 		{
-			long long x; cin>>x;
+			int x; cin>>x;
 			int ans;
-			ans=lower_bound(v,v+n+1,x)-v;
+			ans=lower_bound(s,s+n+1,x)-s;
 			ans>=n+1?cout<<-1<<endl:cout<<ans<<endl;
 		}
 	}
 	return 0;
 }
 		
+			
 			
